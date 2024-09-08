f1a016c0e31234021db68d13e3cc9802e364f10e
Anastasia Gorbunova <elizabethday.8518@icloud.com>
2024-09-08 12:53:15 +0300

    относительный путь

--- analysis.py
+++ analysis.py
@@ -1,2 +1,11 @@
-# heh
-# gokjotgkgotk
+import pandas as pd
+import os
+
+# Путь к файлу относительно местоположения текущего скрипта
+base_dir = os.path.dirname(__file__)  # Получение пути к директории, где находится текущий скрипт
+file_path = os.path.join(base_dir, 'psyche_drug')  # Построение полного пути к файлу, то есть к части пути base_dir добавляется 'psyche_drug'
+
+# Чтение CSV файла
+data = pd.read_csv(file_path)
+# print(data.head())
+
