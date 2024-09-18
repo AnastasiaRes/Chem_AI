7fe20fc08584bb49168729f4fed555f6737536d8
Oglogot <adalatyp@gmail.com>
2024-09-18 14:08:46 +0300

    Сделала корреляции с целевой переменной

--- Corellation_Graphs.py
+++ Corellation_Graphs.py
@@ -11,6 +11,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np 
 from sklearn.feature_selection import VarianceThreshold
+from scipy.stats import pearsonr
+from scipy.stats import spearmanr
 
 # Получаем путь к директории, где находится скрипт
 base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -60,12 +62,50 @@ plt.figure(figsize = (20, 16))
 sns.heatmap(df_variance.corr(), annot=False, cmap='coolwarm', fmt=".2f")
 plt.title('Correlation Heatmap of Reduced Dataset 2')
 
+is_psychedelic = 'is_psychedelic'
+correlations = df_variance.corr()[is_psychedelic].drop(is_psychedelic)
+
+# Создаем точечные графики для каждой переменной
+features = df_variance.columns.drop(is_psychedelic)
+for feature in features:
+    plt.figure(figsize=(8, 6))
+    sns.scatterplot(data=df_variance, x=feature, y=is_psychedelic)
+    correlation, p_value = pearsonr(df_variance[feature], df_variance[is_psychedelic])
+    plt.title(f'Корреляция Пирсона между {feature} и {is_psychedelic}')
+    plt.xlabel(feature)
+    plt.ylabel(is_psychedelic)
+    plt.grid(True)
+    plt.text(0.05, 0.5, f'Корреляция: {correlation:.2f}, p-значение: {p_value:.4f}', 
+             horizontalalignment='left', 
+             verticalalignment='center', 
+             transform=plt.gca().transAxes, 
+             fontsize=12, 
+             bbox=dict(facecolor='white', alpha=0.5))
+    plt.show()
+    
+# Создаем точечные графики для каждой переменной по спирмену
+correlations = df_variance.corr(method='spearman')[is_psychedelic].drop(is_psychedelic)
+for feature in features:
+    plt.figure(figsize=(8, 6))
+    sns.scatterplot(data=df_variance, x=feature, y=is_psychedelic, color='red')
+    correlation, p_value = spearmanr(df_variance[feature], df_variance['is_psychedelic'])
+    plt.title(f'Корреляция Спирмена между {feature} и {is_psychedelic}')
+    plt.xlabel(feature)
+    plt.ylabel(is_psychedelic)
+    plt.grid(True)
+    plt.text(0.05, 0.5, f'Корреляция: {correlation:.2f}, p-значение: {p_value:.4f}', 
+             horizontalalignment='left', 
+             verticalalignment='center', 
+             transform=plt.gca().transAxes, 
+             fontsize=12, 
+             bbox=dict(facecolor='white', alpha=0.5))
+    plt.show()
+
 # Отбор коррелирующих признаков
 corr_matrix = df_variance.corr().abs()
 upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
 to_drop = [column for column in upper.columns if 0.6 > any(upper[column] > 0.8)]
 df_variance.drop(to_drop, axis=1, inplace=True)
-
 
 df_variance['is_psychedelic'] = df['is_psychedelic']
 
